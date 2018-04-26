2ae25118b3d177956543e77500e23fc1015c9f47
Robert Brown <robrownn@gmail.com>
2018-04-26 18:58:05 -0400

    Created BuildClass model

---
+++ PoE-OBM/Models/BuildClass.cs
@@ -0,0 +1,13 @@
+namespace PoE_OBM.Models
+{
+    public enum BuildClass
+    {
+        Duelist,
+        Marauder,
+        Ranger,
+        Scion,
+        Shadow,
+        Templar,
+        Witch
+    }
+}

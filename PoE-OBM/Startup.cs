63b22960998b87b8660fff9face202a4a7454248
Robert Brown <robrownn@gmail.com>
2018-04-25 21:44:25 -0400

    Added port mapping to docker compose, changed db connection to point to local db

--- PoE-OBM/Startup.cs
+++ PoE-OBM/Startup.cs
@@ -27,7 +27,7 @@ namespace PoE_OBM
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DockerConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
